d87bb5112e943cc617f378f56a7b63681c49bada
Daniel Dahme <danieldahme@googlemail.com>
2026-04-13 20:53:01 +0000

    refactoring

--- src/ContentControl/WebsiteContentControl.php
+++ src/ContentControl/WebsiteContentControl.php
@@ -4,6 +4,7 @@ namespace ManageWebsite\ContentControl;
 
 use Base3\Api\IAssetResolver;
 use Base3\Api\IMvcView;
+use Base3\LinkTarget\Api\ILinkTargetService;
 use Base3Manager\Service\Base3Manager;
 use Base3Manager\ContentControl\AbstractContentControl;
 
@@ -12,9 +13,10 @@ class WebsiteContentControl extends AbstractContentControl {
 	public function __construct(
 		protected IMvcView $view,
 		protected Base3Manager $base3manager,
+		protected ILinkTargetService $linktargetservice,
 		private readonly IAssetResolver $assetResolver
 	) {
-		parent::__construct($view, $base3manager);
+		parent::__construct($view, $base3manager, $linktargetservice);
 	}
 
         // Implementation of IBase
@@ -37,4 +39,3 @@ class WebsiteContentControl extends AbstractContentControl {
 		$this->view->assign('resolve', fn($src) => $this->assetResolver->resolve($src));
 	}
 }
-
